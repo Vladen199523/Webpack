403cf4bbcf3404c148f819e40a0fabb6745a383e
Vladen199523 <Vladen199523@yandex.by>
2021-10-26 11:26:46 +0300

    webpack dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  
   entry: path.resolve(__dirname, 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
